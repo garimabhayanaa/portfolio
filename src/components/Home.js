26ea915e6096b92dd74544c6084e37e81cbbd3bb
Garima Bhayana <garimaabhayanaa@gmail.com>
2024-10-15 03:00:41 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,33 @@
 import "../stylesheets/Home.css";
+import { useEffect, useState } from "react";
 
 function Home() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Detect screen size changes
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    };
+    
+    handleResize(); // Check on initial load
+    window.addEventListener("resize", handleResize); // Add resize listener
+    
+    return () => window.removeEventListener("resize", handleResize); // Cleanup on unmount
+  }, []);
+
   return (
     <div className="home-content">
-      <h1 id="name"> &lt; Garima Bhayana /&gt; </h1>
-      <h2 id="profession">AI/ML Engineer | Full Stack Developer</h2>
+      <h1 id="name">
+        <span className="first-name">&lt; Garima</span>
+        {isMobile ? <br /> : " "}
+        <span className="last-name">Bhayana /&gt;</span>
+      </h1>
+      <h2 id="profession">AI/ML and Full Stack Software Engineer</h2>
     </div>
   );
 }
