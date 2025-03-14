5df0ea7ead26c058de3c68b86531d85b2a55fddd
garimabhayanaa <garimaabhayanaa@gmail.com>
2025-03-15 00:57:44 +0530

    Entire portfolio updated.

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import Bar from './Bar';
-import Home from './Home'
-import Journey from './Journey';
-import Toolbox from './ToolBox';
-import Creations from './Creations';
-import Climb from './Climb';
-import DownloadResume from './DownloadResume';
-import LetsConnect from './Socials';
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import Homepage from './Homepage';
+import Experience from './Experience';
+import Projects from './Projects';
+import Resume from './Resume';
+import About from './About';
+import Socials from './Socials';
+import ToolBox from './ToolBox';
+import Chatbot from './Chatbot'
+import '../stylesheets/App.css';
 
-function App () {
+function App() {
+  const [activeSection, setActiveSection] = useState('homepage');
+
+  // Map of section ids to their component
+  const sections = {
+    homepage: <Homepage setActiveSection={setActiveSection} />,
+    about: <About />,
+    skills: <ToolBox />,
+    experience: <Experience />,
+    projects: <Projects />,
+    resume: <Resume />,
+    socials: <Socials />, 
+  };
+
   return (
-    <div>
-      <Bar />
-      <Home />
-      <Journey />
-      <Toolbox />
-      <Creations />
-      <Climb />
-      <DownloadResume />
-      <LetsConnect />
+    <div className="app">
+      <Navbar activeSection={activeSection} setActiveSection={setActiveSection} />
+      <main className="content">
+        {sections[activeSection]}
+      </main>
+      <Chatbot/>
     </div>
   );
-};
+}
 
 export default App;
